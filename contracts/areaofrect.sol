6abd2909b34f06668833771537682a442c271b64
Vignesh0713 <vigneshdumby0713@gmail.com>
2023-11-01 11:44:34 +0530

    initial commit

---
+++ contracts/areaofrect.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.4.18;
+
+contract Areaofrectangle{
+uint result;
+function Rect(uint _l , uint _b) public view returns (uint){
+ 
+result = _l * _b;
+
+return result;
+
+}
+
+}
---
+++ contracts/areaofsquare.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.4.18;
+
+contract Areaofsquare{
+
+uint result;
+function Aos(uint _1 ) public view returns (uint){
+ 
+result = _1 * _1 ;
+
+return result;
+
+}
+
+}
